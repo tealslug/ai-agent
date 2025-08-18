36063c5e8e7f6bed84a766ae79d20afda2c48f2a
tealslug <tealslug@lionsoft.com>
2025-08-17 20:25:08 -0700

    Convert to using messages to allow for future addition of conversations.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,8 +15,14 @@ def main():
         print("Only argument must be the prompt")
         sys.exit(1)
 
-    prompt = sys.argv[1]
-    resp = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    resp = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
     print(resp.text)
     print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
